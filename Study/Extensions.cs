3502622fad4c2d8027ea8638966aea2501217bfe
Thiago <thiagojsf@hotmail.com>
2019-03-08 23:41:07 -0300

    Static members

---
+++ Study/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    public static class Extensions
+    {
+
+        private static int HasLanguagesCount = 0;
+
+        public static bool HasLanguages(this Programmer programmer)
+        {
+            HasLanguagesCount++;
+            return programmer.Languages.Count() > 0;
+        }
+    }
+}
