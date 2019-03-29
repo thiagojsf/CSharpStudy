0d69ed1b73c7e697f7c6eab09b66f2fec667f753
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2019-03-28 23:56:30 -0300

    Desenvolvimento
    Criação da classe IgorAfonso e desenvolvimento das propriedades da classe Banco

---
+++ Study/Students/IgorAfonso.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Study.Students
+{
+    class IgorAfonso : Programmer
+    {
+        public IgorAfonso()
+        {
+            Name = "Igor Afonso Nunes";
+            Birthday = new DateTime(1999, 01, 18);
+            PersonalAddress = new Address()
+            {
+                Street = "Av. Barão Homem de Melo",
+                Number = 3280,
+                ZipCode = "30494-080",
+                City = "Belo Horizonte",
+                Province = "Minas Gerais",
+                Country = "Brasil"
+            };
+            Languages = new List<Language>
+            {
+                new Language
+                {
+                    Name = "C#",
+                    IsFunctional = true,
+                    IsOO = true
+                },
+                new Language
+                {
+                    Name = "Java",
+                    IsFunctional = true,
+                    IsOO = true
+                },
+                new Language
+                {
+                    Name = "HTML",
+                    IsFunctional = false,
+                    IsOO = false
+                },
+            };
+        }
+    }
+}
